a2280fdbe7fcd59b10ca4ed1d81adef1569d2c1b
Teeruth Ieowsakulrat <teeruth2546@gmail.com>
2023-03-21 13:26:16 +0700

    <fix>:Fix attribute in class

--- Room.py
+++ Room.py
@@ -1,12 +1,12 @@
 class Room():
-    def __init__(self):
-        self.__room_type = []
-        self.__price_room = []
-        self.__facilities_detail = []
-        self.__bed_type = []
-        self.__room_picture = []
-        self.__room_number = []
-        self.__room_status = []
+    def __init__(self, room_type, price_room, facilities_detail, bed_type, room_picture, room_number, room_status):
+        self.__room_type = room_type
+        self.__price_room = price_room
+        self.__facilities_detail = facilities_detail
+        self.__bed_type = bed_type
+        self.__room_picture = room_picture
+        self.__room_number = room_number
+        self.__room_status = room_status
     def select_filters_room(self):
         pass
     def select_addons(self):
@@ -17,30 +17,30 @@ class Room():
         pass
 
 class Addons:
-    def __init__(self):
-        self.__detail = []
+    def __init__(self, detail):
+        self.__detail = detail
         self.__add_on_list  =[]
     def get_addons(self):
         pass
 
 class BreakfastService():
-    def __init__(self):
-        self.__type_food = []
-        self.__price_food = []
+    def __init__(self, type_food, price_food):
+        self.__type_food = type_food
+        self.__price_food = price_food
 
 class SpaService:
-    def __init__(self):
-        self.__reserve_spa = []
-        self.__price_service = []
+    def __init__(self, reserve_spa, price_service):
+        self.__reserve_spa = reserve_spa
+        self.__price_service = price_service
 
 class ActivityService:
-    def __init__(self):
-        self.__data_activity = []
-        self.__price_activity = []
-        self.__num_person = []
+    def __init__(self, date_activity, price_activity, num_person):
+        self.__data_activity = date_activity
+        self.__price_activity = price_activity
+        self.__num_person = num_person
 
 class TaxiService:
-    def __init__(self):
-        self.__number_taxi = []
-        self.__fees_taxi = []
-        self.__contact_taxi = []
+    def __init__(self, number_taxi, fees_taxi, contact_taxi):
+        self.__number_taxi = number_taxi
+        self.__fees_taxi = fees_taxi
+        self.__contact_taxi = contact_taxi
