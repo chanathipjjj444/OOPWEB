d2d1f3fc7d58beeb07ece866cdcbdf71e2acc4a5
Jay <chanathip2320@gmail.com>
2023-04-15 09:06:22 +0700

    splitfile

--- Payment.py
+++ Payment.py
@@ -1,29 +1,101 @@
+from room import Booking
+
+class Promotion:
+    def __init__(self):
+        self.total_coupon= [] #list of Typecoupon
+        self.total_discount_num_room_list = [] #list of discountnumroom
+
+    def add_coupon(self, coupon):
+        self.total_coupon.append(coupon)
+
+    def show_coupon_list(self):
+        for discount in self.total_coupon:
+            print("Total Coupon:",self.total_coupon)
+    
+    def get_coupon_list(self):
+        return self.total_coupon
+
 class Payment:
-    def __init__(self, total, transaction_id, payment_method, card_number, card_name, card_expire, cvv_card, vat):
-        self.__total=total
+    def __init__(self, transaction_id, payment_method):
         self.__transaction_id = transaction_id
         self.__payment_method = payment_method
-        self.__card_number = card_number
-        self.__card_name = card_name
-        self.__card_expire = card_expire
-        self.__cvv_card = cvv_card
-        self.__vat = vat
+        self.__money = 0 
+        self.__price_room = 0
+        self.__status_payment =False
+        self.__used_coupons = []
+        self.__total_day = 0
+        self.__total_price = 0
+        self.__add_on_price = 0
+    
+    def set_price_room(self, price_room):
+        if isinstance(price_room,int):
+            self.__price_room = price_room
+    
+    def set_money(self,balance):
+        self.__money += balance
+    
+    def get_price_room(self):
+        return self.__price_room
+    
     def payment_perform(self):
         pass
+    
     def get_payment_status(self):
-        pass
-class Promotion:
-    def __init__(self, typecoupon, discountnumroom):
-        self.__total_coupon= typecoupon #list of Typecoupon
-        self.__total_discount_num_room_list = discountnumroom #list of discountnumroo 
+        return self.__status_payment
+    
+    def set_total_day(self, difference):
+        self.__total_day += difference
+        print("Total day:",self.__total_day)
+
+    def set_add_on_price(self, add_on_price, book1=Booking):
+        self.__add_on_price = book1.service_price
+        print("Add on price:",self.__add_on_price)
+
+    def set_total_price(self):
+        self.__total_price = (self.__price_room * self.__total_day) + self.__add_on_price
+        print("Total price:",self.__total_price)
+
+    def use_coupon(self, promotion : Promotion):
+        print("Do you want to use coupon:")
+        requirement = input("True or False:")
+        if requirement == "True":
+            name_coupon = input("Enter coupon code:")
+            for coupon in promotion.total_coupon:
+                if coupon.name_coupon == name_coupon:
+                    self.__total_price  -= coupon.value_coupon
+                    break
+            else:
+                print("Invalid coupon name")
+        else: 
+            print("Not use coupon")
+
+    def process_payment(self):
+        print("Process Payment...........")
+        print("......")
+        print("...")
+        if(self.__money >= self.__total_price): #price_room must multiply day checkout-checkin
+
+            if self.use_coupon:
+                print("Total after use coupon code:",self.__total_price)
+            
+            print("Success Payment!!")
+            self.__money-=self.__total_price
+            self.__status_payment = True
+            print("Payment status is True")
+            print("Your Balance is",self.__money)
+            return True
+        else:
+            print("Fail Payment")
+            return False
+
+
 
 class TypeCoupon:
-    def __init__(self, value_coupon, pr, coupon_description):
-        self.__value_coupon = value_coupon
-        self.__pr=pr
-        self.__coupon_description=coupon_description
-
+    def __init__(self, name_coupon, coupon_description, value_coupon):
+        self.name_coupon = name_coupon
+        self.coupon_description=coupon_description
+        self.value_coupon = value_coupon
 class DiscountNumRoom:
     def __init__(self, num_room_discount, value_discount):
-        self.__num_room_discount = num_room_discount
-        self.__value_discount = value_discount
+        self.num_room_discount = num_room_discount
+        self.value_discount = value_discount
---
+++ creditcard.py
@@ -0,0 +1,72 @@
+class Catalog_creditcard:
+    def __init__(self):
+        self.creditcard_list = ["100-Prayad-456-10000","101-Messi-555-5000","102-John-789-7000","103-Man-858-500"]
+        self.card_number_list =[]
+        self.card_name_list=[]
+        self.cvv_card_list=[]
+        self.balance_list =[]
+    
+    def manage_card(self):
+        for i in self.creditcard_list:
+            collect = i.split("-")
+            self.card_number_list.append(collect[0])
+            self.card_name_list.append(collect[1])
+            self.cvv_card_list.append(collect[2])
+            self.balance_list.append(int(collect[3]))
+
+    def add_card(self, card):
+        self.creditcard_list.append(card)
+
+
+class Creditcard():
+    def __init__(self, card_number, card_name, cvv_card):
+        self.__card_number = card_number
+        self.__card_name = card_name
+        self.__cvv_card = cvv_card
+        self.__card_number_list = [] #check card num
+        self.__card_name_list = [] #check card name
+        self.__balance_list = [] #check balance
+        self.__balance = 0
+        self.__check_status_access = False
+        self.__check_end = 0
+
+    def get_card_number(self):
+        return self.__card_number
+    
+    def get_card_name(self):
+        return self.__card_name
+    
+    def get_cvv_card(self):
+        return self.__cvv_card
+    
+    def get_balance_in_card(self):
+        return self.__balance
+    
+    #print check
+    def get_data_card_list(self):
+        return self.__card_number_list
+    
+    def receive_data_card(self, check_number, check_name):
+        self.__card_number_list += check_number
+        self.__card_name_list += check_name
+
+    def check_credit_card(self):
+        check_position = -1
+        for i in self.__card_number_list:
+            check_position +=1
+            if(int(i)==int(self.__card_number)):
+                print("ACCEPT CREDIT CARD!!")
+                self.__check_status_access=True
+                self.__check_end = check_position
+            else:
+                print("ERRORRRRR NOT FOUND NUMBER CARD")
+            
+    
+    def balance_in_card(self, balance):
+        position_balance = 0
+        if self.__check_status_access:
+            position_balance = self.__check_end
+            self.__balance += int(balance[position_balance])
+            print("Success access balance")
+        else:
+            print("Error Insert Money!!!!")
