37ac748cd5a58ac712c6ee62fea3d7c6d99a027f
Teeruth Ieowsakulrat <93991106+teeruth09@users.noreply.github.com>
2023-04-18 01:41:04 +0700

    <fix>:Add function add_room

--- Hotel.py
+++ Hotel.py
@@ -1,7 +1,12 @@
-#everything in this is public
+from fastapi import FastAPI
+from typing import List
+from pydantic import BaseModel
+
+
+app = FastAPI()
 class Hotel:
-    def __init__(self, name, rating, num_rooms, hotel_picture, location, province, room_list, add_on_hotel):
-        self.name_hotel = name
+    def __init__(self, name_hotel, rating, num_rooms, hotel_picture, location, province, room_list, add_on_hotel):
+        self.name_hotel = name_hotel
         self.rating = rating
         self.num_rooms = num_rooms
         self.hotel_picture = hotel_picture
@@ -16,12 +21,25 @@ class Hotel:
                   "price:",room.price_room, "facilities:",room.facilities_detail, "bed type:",room.bed_type, "status:",room.room_status)
     def show_add_on(self):
         for add_on in self.add_on_hotel:
-            print("Addon :",add_on.add_on_list)            
-    '''
-    def show_add_on(self, addon_object):
-        for add_on in addon_object.add_on_list:
-            print("Addon in hotel:",addon_object.add_on_list)
-    '''
+            print("Addon in this Hotel:",add_on.add_on_list)  
+
+    def show_hotel(self,hotel_catalog:object):
+        self.hotel_catalog = hotel_catalog
+        for hotel in self.hotel_catalog.hotel_list:
+            print("Name hotel:",hotel.name_hotel, "Rating:",hotel.rating,"Num_rooms:",hotel.num_rooms,
+                  "hotel_picture:",hotel.hotel_picture, "Location:",hotel.location, "Province:",hotel.province)
+            
+    #add function add room
+    def add_room(self, room):
+        self.room_list.append(room)
+        return {"message": "Room added successfully", "room_list": self.room_list}
+    #add function add add on
+    def add_addons(self,add_on):
+        self.add_on_hotel.append(add_on)
+        return {"message": "Addon added successfully", "add_on_hotel": self.add_on_hotel}
+    
+    
+    
 class HotelCatalog:
     def __init__(self):
         self.hotel_list = []
@@ -36,3 +54,18 @@ class HotelCatalog:
                 return hotel
         print("Error: Hotel not found")
         return None
+    
+    def find_available_room(self, name):
+        self.available_rooms = []
+        hotel =self.find_hotel(name)
+
+        if hotel:
+            for room in hotel.room_list:
+                if room.room_status == True:
+                    self.available_rooms.append(room)
+            return  self.available_rooms
+        else:
+            return None
+    
+    
+catalog = HotelCatalog()
