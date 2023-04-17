fd85793816d3b635bf331ea73a35e6933b1af2f9
Tung <tung542547@gmail.com>
2023-04-17 11:44:39 +0700

    <fix>:Fix attribute in class

--- Account.py
+++ Account.py
@@ -1,3 +1,6 @@
+from fastapi import FastAPI
+
+app = FastAPI()
 class Profile:
     def __init__(self, prefix, name, surname, email, phone_number):
         self._prefix = prefix
@@ -8,48 +11,89 @@ class Profile:
 
 class User:
     def __init__(self, name, id, password):
-        self._id = id
-        self._password = password
-        self._name = name
-        self._user_list =[]
-    def add_user(self, user):
-        self._user_list.append(user)
+        self.__id = id
+        self.__password = password
+        self.__name = name
+
+    @property
+    def name(self):
+        return self.__name
+    
+    @property
+    def id(self):
+        return self.__id
+    
+    @property
+    def password(self):
+        return self.__password
 
 class Admin:
     def __init__(self, name, id, password):
-        self._id = id
-        self._password = password
-        self._name = name
-        self._admin_list =[]
-    def add_admin(self, admin):
-        self._admin_list.append(admin)
+        self.__id = id
+        self.__password = password
+        self.__name = name
 
+    @property
+    def name(self):
+        return self.__name
+    
+    @property
+    def id(self):
+        return self.__id
+    
+    @property
+    def password(self):
+        return self.__password
+    
 class Login:
-    def __init__(self, id, password):
-        self._id = id
-        self._password = password
-        self.admin_list = []
-        self.user_list = []
-    def check_id(self, admin_list, user_list):
-        self.admin_list = admin_list
-        self.user_list = user_list
-        for admin in self.admin_list:
-            if self._id == admin._id and self._password == admin._password:
+    def __init__(self):
+        self.__admin_list = []
+        self.__user_list = []
+    
+    @property
+    def user_list(self):
+        return self.__user_list
+
+    def add_user(self, user):
+        self.__user_list.append(user)
+
+    def add_admin(self, admin):
+        self.__admin_list.append(admin)
+
+    def check_id(self, account):
+        for admin in self.__admin_list:
+            if admin.id == account.id and admin.password == account.password:
                 return print("ซ้ำโว้ยยย")
             else:
-                return print(admin._id, admin._password)
-        for user in self.user_list:
-            if self._id == user._id and self._password == user._password:
+                return print(account.id, account.password)
+        for user in self.__user_list:
+            if user.id == account.id and user.password == account.password:
                 return print("ซ้ำโว้ยยย")
             else:
-                return print(user._id, user._password)
+                return print(account.id, account.password)
+
+    def show_detail(self, account):
+        view_account = {
+            "name" : account.name,
+            "email" : account.id,
+            "password" : account.password
+        }
+        return view_account
 
 
 user = User("Tung","kiki123@gmail.com","12345678")
-user.add_user(user)
 admin = Admin("Tung","kiki123@gmail.com","1234567")
-admin.add_admin(admin)
 
-login = Login(user._id,user._password)
-login.check_id(admin._admin_list,user._user_list)
-# print(user._user_list)
+login = Login()
+login.add_user(user)
+login.add_admin(admin)
+login.check_id(user)
+login.check_id(admin)
+
+@app.get("/create_account/{name}")
+async def get_user(name:str):
+    for detail in login.user_list:
+        if  detail.name == name:
+            return login.show_detail(detail)
+
+print(login.show_detail(user))
