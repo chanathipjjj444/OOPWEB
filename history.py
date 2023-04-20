0b3e107341ebe233ac8f365189df41e064aca834
Teeruth Ieowsakulrat <93991106+teeruth09@users.noreply.github.com>
2023-04-21 01:14:19 +0700

    <add>:Add class OrderHistory

---
+++ history.py
@@ -0,0 +1,12 @@
+class OrderHistory:
+    def __init__(self):
+        self.history = []
+        
+    def show_history(self):
+        for history in self.history:
+            print("Date check in :",history.check_in, 
+                  "Date check out:",history.check_out, 
+                  "Number people:",history.num_people, 
+                  "Total room:",history.num_room, 
+                  "Hotel name:",history.hotel_name, 
+                  "Room number:",history.room_number)
