3b5b444e47d314bbcf2af1e7019ee74b70a375b4
TungEiEi <110532849+TungEiEi@users.noreply.github.com>
2023-04-21 16:45:37 +0700

    Update Account.py

--- Account.py
+++ Account.py
@@ -59,22 +59,30 @@ class Login:
         return self.__user_list
 
     def add_user(self, user):
-        self.__user_list.append(user)
+        if type(self.check_id(user)) != str:
+            self.__user_list.append(user)
 
     def add_admin(self, admin):
-        self.__admin_list.append(admin)
+        if type(self.check_id(admin)) != str:
+            self.__admin_list.append(admin)
 
     def check_id(self, account):
         for admin in self.__admin_list:
-            if admin.id == account.id and admin.password == account.password:
-                return print("ซ้ำโว้ยยย")
+            if admin.id != account.id:
+                if admin.id == account.id and admin.password == account.password:
+                    return "ซ้ำโว้ยยย"
+                else:
+                    return account
             else:
-                return account
+                return "ID ซ้ำาา"
         for user in self.__user_list:
-            if user.id == account.id and user.password == account.password:
-                return print("ซ้ำโว้ยยย")
+            if user.id != account.id:
+                if user.id == account.id and user.password == account.password:
+                    return "ซ้ำโว้ยยย"
+                else:
+                    return account
             else:
-                return account
+                return "ID ซ้ำาา"
 
     def show_detail(self, account):
         view_account = {
@@ -86,19 +94,20 @@ class Login:
 
 
 user = User("Tung","kiki123@gmail.com","12345678")
+user2 = User("Tung","kiki123@gmail.com","12345678")
 admin = Admin("Tung","kiki123@gmail.com","1234567")
 
 login = Login()
 login.add_user(user)
-login.add_admin(admin)
-login.check_id(user)
-login.check_id(admin)
+login.add_admin(user)
+login.add_user(user2)
+print(login.user_list)
 
-@app.get("/create_account/{name}")
-async def get_user(name:str):
-    for detail in login.user_list:
-        if  detail.name == name:
-            return login.show_detail(detail)
+# @app.get("/create_account/{name}")
+# async def get_user(name:str):
+#     for detail in login.user_list:
+#         if  detail.name == name:
+#             return login.show_detail(detail)
         
 @app.get("/list_account")
 async def get_list():
@@ -109,24 +118,30 @@ async def webLogin(request: Request):
     page_data = {"request": request}
     return TEMPLATE.TemplateResponse("login.html", page_data)
 
+@app.post("/check_login",response_class=HTMLResponse)
+async def webLogin(request: Request):
+    page_data = {"request": request}
+    return TEMPLATE.TemplateResponse("login.html", page_data)
+
 @app.get("/register",response_class=HTMLResponse)
 async def WebRegister(request: Request):
     page_data = {"request": request}
     return TEMPLATE.TemplateResponse("register.html", page_data)
 
 @app.get("/add_account",response_class=HTMLResponse)
-async def check(request: Request, name, email, password):
-    account = User(name, email, password)
+async def add(request: Request, name, email, password):
+    user = User(name, email, password)
+    admin = Admin(name, email, password)
+    login.add_user(user)
+    login.add_admin(admin)
     page_data = {"request": request}
-    login.user_list.append(login.check_id(account))
-    return TEMPLATE.TemplateResponse("register.html", page_data)
+    return TEMPLATE.TemplateResponse("login.html", page_data)
 
-
-@app.post("/list_account")
-async def add_account(name, id, password):
-    account = User(name, id, password)
-    login.user_list.append(account)
-    return {"Data" : login.user_list}
+# @app.post("/list_account")
+# async def add_account(name, id, password):
+#     account = User(name, id, password)
+#     login.user_list.append(account)
+#     return {"Data" : login.user_list}
 
 # r = requests.get("http://127.0.0.1:8000/list_account")
 # print(r.json())
